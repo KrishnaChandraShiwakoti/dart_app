7f3b0fbf070fb1576ff005b9a936c7f101957c47
Krishna Shiwakoti <rijankrishna14@gmail.com>
2026-04-05 14:09:10 +0545

    Add initial implementations for lab2 questions 1-4

---
+++ lib/lab2/fruits_list.dart
@@ -0,0 +1,7 @@
+// question 2
+void main(List<String> args) {
+  List<String> fruits = ["Apple", "Kiwi", "DragonFruit", "Banana"];
+  for (var fruit in fruits) {
+    print(fruit);
+  }
+}
---
+++ lib/lab2/name_list.dart
@@ -0,0 +1,7 @@
+// question 1
+void main(List<String> args) {
+  List<String> names = ["Rijan", "Bishal", "Kripan", "Pradip"];
+  for (var name in names) {
+    print(name);
+  }
+}
